3defe84b167e31e11c55a5982db85074210706d2
aronpendolema227 <aronpendolema227@gmail.com>
2026-08-03 01:55:20 -0500

    Add product model with stock and catalog information

---
+++ app/src/main/java/com/ap/cartly/model/Product.kt
@@ -0,0 +1,32 @@
+package com.ap.cartly.model
+
+data class Product(
+    val id: String,
+    val name: String,
+    val price: String,
+    private var stockDisponible: Int,
+
+    // Informacion necesaria para la interfas
+    val category: String = "General",
+    val description: String = "",
+    val specifications: List<String> = emptyList(),
+    val imageResId: Int? = null
+){
+    val stock: Int
+        get() = stockDisponible
+
+    val estaDisponible: Boolean
+        get() = stockDisponible > 0
+
+    fun tieneStock(cantidad: Int): Boolean {
+        return cantidad > 0 && cantidad <= stockDisponible
+    }
+    fun descontarStock(cantidad: Int): Boolean{
+        if (!tieneStock(cantidad)) {
+            return false
+        }
+
+        stockDisponible -= cantidad
+        return true
+    }
+}
