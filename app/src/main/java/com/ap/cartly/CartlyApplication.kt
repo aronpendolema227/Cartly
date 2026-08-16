99c89c96a576245847f3adde093f3fc2f9df22d9
aronpendolema227 <aronpendolema227@gmail.com>
2026-08-16 18:16:12 -0500

    feat: configura CartlyApplication y carga inicial

---
+++ app/src/main/java/com/ap/cartly/CartlyApplication.kt
@@ -0,0 +1,38 @@
+package com.ap.cartly
+
+import android.app.Application
+import com.ap.cartly.data.local.CartlyDatabase
+import com.ap.cartly.data.repository.ProductRepository
+import com.ap.cartly.data.repository.UserRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+
+class CartlyApplication : Application() {
+
+    val database by lazy {
+        CartlyDatabase.getDatabase(this)
+    }
+
+    val productRepository by lazy {
+        ProductRepository(database.productDao())
+    }
+
+    val userRepository by lazy {
+        UserRepository(database.userDao())
+    }
+
+    private val applicationScope =
+        CoroutineScope(SupervisorJob() + Dispatchers.IO)
+
+    override fun onCreate() {
+        super.onCreate()
+
+        applicationScope.launch {
+            productRepository.initializeProductsIfNeeded()
+            userRepository.initializeUsersIfNeeded()
+        }
+    }
+}
+
